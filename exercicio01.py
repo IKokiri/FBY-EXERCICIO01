1c5ed6254e0502f7f09cf3b479cf1c26eda0945d
Luiz Augusto Mendes Silva <888004@sga.pucminas.br>
2020-03-03 22:04:10 -0300

    exe1

--- exercicio01.py
+++ exercicio01.py
@@ -1,7 +1,15 @@
-#Set Coleção não ordenada sem duplicidade
+#Outras estruturas de dados
+#1.1 Set Coleção não ordenada sem duplicidade
+open("text.txt", "r")
 lista = [1,2,3,4,5,6,7,8,9]
 print(set(lista))
 
-#Tuple: Alguns valores separados por virgula
+#1.2 Tuple: Alguns valores separados por virgula
 tupla = (1,2,3,4,5,6,7,8,9)
 print(tupla)
+
+#2 Como usar listas
+#2.1 como uma fila (ou queue)
+
+    
+
